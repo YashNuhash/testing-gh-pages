c452afed3d4673741335dc50c3f5b5e1e3ac68fa
YashNuhash <nuhashroxme@gmail.com>
2023-12-10 12:06:28 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello Peter !!!</h1>
+     <h3>Testing for GitHub pages & GitHub Actions</h3>
+    </>
+  )
+}
+
+export default App
